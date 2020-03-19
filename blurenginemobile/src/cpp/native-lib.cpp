1e3e23fdd4f7f7558dbab689d3385193b2839a51
Functionable <40835042+Functionable@users.noreply.github.com>
2020-03-19 22:34:36 +0000

    JNI API tests.

--- blurenginemobile/src/cpp/native-lib.cpp
+++ blurenginemobile/src/cpp/native-lib.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_qprogramming_bluregine_MainActivity_stringFromJNI(
+Java_com_qprogramming_blurenginemobile_MainActivity_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
+        jclass javaclass) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
--- blurenginemobile/src/main/java/com/qprogramming/blurenginemobile/MainActivity.java
+++ blurenginemobile/src/main/java/com/qprogramming/blurenginemobile/MainActivity.java
@@ -3,12 +3,20 @@ package com.qprogramming.blurenginemobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public static native String stringFromJNI();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = (TextView)findViewById(R.id.label);
+        text.setText(stringFromJNI());
     }
 }
